264b2ce9ba42e8153ff2f63f48e0213451ca8d58
pbh914 <52387364+ArkBB@users.noreply.github.com>
2025-05-26 01:00:04 +0900

    feat : 기본 멤버 세팅

---
+++ src/main/java/com/example/chatserver/member/domain/Member.java
@@ -0,0 +1,39 @@
+package com.example.chatserver.member.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true,nullable = false)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public Long getId() {
+        return id;
+    }
+
+
+}
