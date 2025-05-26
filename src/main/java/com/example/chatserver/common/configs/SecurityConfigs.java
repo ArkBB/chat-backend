b15c1969d23bbfc3d3d26e3caa569c4cf08209ef
pbh914 <52387364+ArkBB@users.noreply.github.com>
2025-05-26 15:34:33 +0900

    feat : 검증 필터
    1. cors,csrf 설정
    2. HTTP Basic, 세션 방식 비활성화
    3. http request 중 로그인, 회원가입의 경우 허용, 그 외의 케이스 검증 인증 필요
    4. 요청에 담긴 토큰을 검증하는 로직을 FilterChain 이전에 걸기 -> jwtAuthFilter

---
+++ src/main/java/com/example/chatserver/common/configs/SecurityConfigs.java
@@ -0,0 +1,54 @@
+package com.example.chatserver.common.configs;
+
+import com.example.chatserver.common.auth.JwtAuthFilter;
+import java.util.Arrays;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class SecurityConfigs {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    public SecurityConfigs(JwtAuthFilter jwtAuthFilter) {
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain AuthFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .cors(cors-> cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)//csrf 방지 비활성화
+                .httpBasic(AbstractHttpConfigurer::disable)//HTTP Basic 비활성화
+                .authorizeHttpRequests(a->a.requestMatchers("/member/create","/member/login")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .sessionManagement(s->s.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //세션방식 사용하지 않음.
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class) //요청에 담긴 토큰을 검증하는 과정을 필터 전에 걸기
+                .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration cs = new CorsConfiguration();
+        cs.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        cs.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        cs.setAllowedHeaders(Arrays.asList("Authorization","Content-Type"));
+        cs.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",cs);
+        return request -> cs;
+    }
+
+
+}
