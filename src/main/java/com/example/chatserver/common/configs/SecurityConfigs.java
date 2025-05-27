6081baca5901a5bb6b197f15d6f1a12b60a50fc1
pbh914 <52387364+ArkBB@users.noreply.github.com>
2025-05-27 16:22:04 +0900

    feat : 토큰 검증

--- src/main/java/com/example/chatserver/common/configs/SecurityConfigs.java
+++ src/main/java/com/example/chatserver/common/configs/SecurityConfigs.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -50,5 +52,9 @@ public class SecurityConfigs {
         return request -> cs;
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
 }
